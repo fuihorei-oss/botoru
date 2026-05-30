d9f46d5dc926865f6679e96da49ebbdd1bdfafb9
fuihorei-oss <fuihorei@gmail.com>
2026-05-30 17:19:33 +0900

    switch to Firebase Realtime Database

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { initializeFirestore, persistentLocalCache } from 'firebase/firestore';
+import { getDatabase } from 'firebase/database';
 import { getAuth, signInAnonymously } from 'firebase/auth';
 
 const firebaseConfig = {
@@ -9,14 +9,11 @@ const firebaseConfig = {
   storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+  databaseURL:       import.meta.env.VITE_FIREBASE_DATABASE_URL,
 };
 
-const app = initializeApp(firebaseConfig);
-
-export const db = initializeFirestore(app, {
-  localCache: persistentLocalCache(),
-});
-
+const app  = initializeApp(firebaseConfig);
+export const db   = getDatabase(app);
 export const auth = getAuth(app);
 
 export async function signInAnon() {
